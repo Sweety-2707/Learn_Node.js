6d66146bdf0d14c85f4bf880b47232534baae032
Sweety Sharma <sweety2020727@gmail.com>
2024-07-17 19:15:47 +0530

    node

--- http_sever.js
+++ http_sever.js
@@ -1,20 +1,50 @@
 const http = require("http");
 const fs = require("fs");
+const url = require("url");
+const express = require("express");
 
-const myServer = http.createServer((req,res)=>{
+
+// 5 http method- GET POST PUT PATCH DELETE 
+
+const app = express();
+// obj.method(path,(req,res)=>{})
+
+app.get('/',(req,res)=>{
+    res.send(`HomePage`);
+})
+
+app.get('/about',(req,res)=>{
+    res.send(`Hello! ${req.query.username}`);
+})
+
+function myHandler(req,res){
+    if(req.url==='/favicon.ico') return res.end();
     console.log(req);
-    let log = `${Date.now()} : Request Received from - ${req.url} \n`;
+    let log = `${Date.now()} : ${req.method}: Request Received from - ${req.url} \n`;
+    const myurl = url.parse(req.url,true);  // parse create object of query parameter if true
     fs.appendFile('log.txt',log,(err,data)=>{
         if(err){
             console.log("Error!");
         }
         else{
-            switch(req.url){
+            console.log(myurl);
+            switch(myurl.pathname){
                 case '/':
                     res.end("Home Page");
                     break;
                 case '/about':
-                    res.end("Hi I am Sweety Sharma");
+                    const username = myurl.query.username
+                    res.end(`Hi, ${username}`);
+                    break;
+                case '/signup':
+                    if(req.method==='GET'){
+                        res.end(`This is thi SignUp form`);
+                    }
+                    else if (req.method==='POST'){
+                        // DataBase Query
+
+                        res.end("Successfully Registered!");
+                    }
                     break;
                 default:
                     res.end("Page No Found!");
@@ -22,6 +52,11 @@ const myServer = http.createServer((req,res)=>{
             }
         }
     })
-});
+}
 
-myServer.listen(8000,()=>console.log("Server Started!"));
+// const myServer = http.createServer(app);
+// const myServer = http.createServer(myHandler);
+
+// myServer.listen(8000,()=>console.log("Server Started!"));
+
+app.listen(8000,()=>console.log(`Server Started!`));
